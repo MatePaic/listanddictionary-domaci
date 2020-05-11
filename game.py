b102fc434165020b8d2a874da23a67b5acff8ab8
matepaic <mate.paic1997@gmail.com>
2020-05-11 13:06:52 +0200

    your message

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import random
 
-player = input("Hi, what is your name? ")
+player = input('Hi, what is your name?')
 
 secret = random.randint(1, 30)
 attempts = 0
@@ -12,21 +12,20 @@ with open("score_list.txt", "r") as score_file:
     print("Top scores: " + str(score_list))
 
 for score_dict in score_list:
-    score_text = "Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}".format(score_dict.get("player_name"),
-                                                                                                                     str(score_dict.get("attempts")),
-                                                                                                                     score_dict.get("date"),
-                                                                                                                     score_dict.get("secret_number"),
-                                                                                                                     score_dict.get("wrong_guesses"))
+    score_text = 'Playr {0} had {1} attempts on {2}. The secret number was {3}.'.format(score_dict.get('player_name'),
+                                                                                        str(score_dict.get('attempts')),
+                                                                                        score_dict.get('date'),
+                                                                                        score_dict.get('secret_number'))
     print(score_text)
 
-wrong_guesses = []
+
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
-        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "player_name": player,
-                           "secret_number": secret, "wrong_guesses": wrong_guesses})
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), 'player_name': player,
+                           'secret_number': secret})
 
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
@@ -38,5 +37,3 @@ while True:
         print("Your guess is not correct... try something smaller")
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
-
-    wrong_guesses.append(guess)
