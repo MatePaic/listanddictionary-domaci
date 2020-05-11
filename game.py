2195e7320e7c022fb2a8cba02037bdc73be70832
matepaic <mate.paic1997@gmail.com>
2020-05-11 13:17:35 +0200

    initial commit

--- game.py
+++ game.py
@@ -12,12 +12,14 @@ with open("score_list.txt", "r") as score_file:
     print("Top scores: " + str(score_list))
 
 for score_dict in score_list:
-    score_text = 'Playr {0} had {1} attempts on {2}. The secret number was {3}.'.format(score_dict.get('player_name'),
-                                                                                        str(score_dict.get('attempts')),
-                                                                                        score_dict.get('date'),
-                                                                                        score_dict.get('secret_number'))
+    score_text = 'Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}'.format(score_dict.get('player_name'),
+                                                                                                                    str(score_dict.get('attempts')),
+                                                                                                                    score_dict.get('date'),
+                                                                                                                    score_dict.get('secret_number'),
+                                                                                                                    score_dict.get('wrong_guesses'))
     print(score_text)
 
+wrong_guesses = []
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
@@ -25,7 +27,7 @@ while True:
 
     if guess == secret:
         score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), 'player_name': player,
-                           'secret_number': secret})
+                           'secret_number': secret, 'wrong_guesses': wrong_guesses})
 
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
@@ -37,3 +39,5 @@ while True:
         print("Your guess is not correct... try something smaller")
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
+
+    wrong_guesses.append(guess)
